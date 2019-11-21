3b2bd866eca465c593007ff6ec53bb55cec2c7a9
TimVanMourik <timvanmourik@gmail.com>
2019-11-22 00:33:55 +0100

    webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = function (env) {
         {
           test: /\.s?css$/,
           loader: 'style-loader!css-loader?modules&camelCase&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader!sass-loader'
-        }
+        },
         {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
